4d4055a7c27bb559154b3202beacc5f0a8fe46a9
cybermaique <maiqued.18@gmail.com>
2022-01-18 09:48:23 -0300

    js revision

--- revisão js/scriptRevisaoJs02.js
+++ revisão js/scriptRevisaoJs02.js
@@ -1,5 +1,5 @@
-import {areaQuadrado, perimetroQuadrado} from './quadrado.js';
+import quadrado from './quadrado.js';
 import numeroAleatorio from 'numeroAleatorio';
 
-console.log(perimetroQuadrado(5));
+console.log(quadrado.perimetroQuadrado(5));
 console.log(numeroAleatorio());
